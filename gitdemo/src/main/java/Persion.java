b494c04fdbec0477f49855bbd2f0798d45ba0a8e
zpc <1573421090@qq.com>
2020-04-14 11:57:04 +0800

    Initial commit

---
+++ gitdemo/src/main/java/Persion.java
@@ -0,0 +1,7 @@
+import lombok.Data;
+
+@Data
+public class Persion {
+    private Integer age;
+    private String name;
+}
---
+++ gitdemo/src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Persion p = new Persion();
+        p.setAge(12);
+        p.setName("胖虎");
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
+    }
+}
